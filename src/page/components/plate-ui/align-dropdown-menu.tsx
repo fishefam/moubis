089fbff17a82a43f0aa705a2ed05e92a70097b0e
Truong Nguyen <trg.mnguyen@gmail.com>
2023-12-23 21:47:58 -0500

    fixed

--- src/page/components/plate-ui/align-dropdown-menu.tsx
+++ src/page/components/plate-ui/align-dropdown-menu.tsx
@@ -1,13 +1,14 @@
-import type {
-  DropdownMenuProps} from '@radix-ui/react-dropdown-menu'
+import type { DropdownMenuProps } from '@radix-ui/react-dropdown-menu'
+import { useAlignDropdownMenu, useAlignDropdownMenuState } from '@udecode/plate'
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
-} from '@radix-ui/react-dropdown-menu'
-import { useAlignDropdownMenu, useAlignDropdownMenuState } from '@udecode/plate'
+} from '@/components'
+import { cn } from '@/lib'
 
 import { Icons, iconVariants } from '../icons'
 import { useOpenState } from './dropdown-menu'
@@ -52,7 +53,14 @@ export function AlignDropdownMenu({ ...props }: AlignDropdownMenuProps) {
       <DropdownMenuContent align='start' className='min-w-0'>
         <DropdownMenuRadioGroup className='flex flex-col gap-0.5' {...radioGroupProps}>
           {items.map(({ value: itemValue, icon: Icon }) => (
-            <DropdownMenuRadioItem key={itemValue} value={itemValue}>
+            <DropdownMenuRadioItem
+              key={itemValue}
+              value={itemValue}
+              className={cn(
+                'hover:bg-slate-100 relative flex select-none items-center rounded-sm text-sm outline-none transition-colors duration-75 focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 h-9 cursor-pointer px-2 data-[state=checked]:bg-accent data-[state=checked]:text-accent-foreground w-16',
+                itemValue === radioGroupProps.value ? 'bg-slate-100' : '',
+              )}
+            >
               <Icon className={iconVariants({ variant: 'toolbar' })} />
             </DropdownMenuRadioItem>
           ))}
